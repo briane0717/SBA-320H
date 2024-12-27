42cc63e1deb3bc89380a16ead36c340cc802f30f
Brian <briane0717@gmail.com>
2024-12-27 13:40:39 -0500

    new idea

--- SBA-320H/src/App.jsx
+++ SBA-320H/src/App.jsx
@@ -1,34 +1,11 @@
-import "./App.css";
+import React from "react";
 
-// Sample products
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Express Yourself!</h1>
-
-      {/* Product List */}
-      <div className="product-list">
-        <div className="product-item">
-          <h3>Mug</h3>
-          <p>$$30.00</p>
-          <button>Add to Cart</button>
-        </div>
-
-        <div className="product-item">
-          <h3>Hoodie</h3>
-          <p>$50.00</p>
-          <button>Add to Cart</button>
-        </div>
-
-        <div className="product-item">
-          <h3>Cup</h3>
-          <p>$20.00</p>
-          <button>Add to Cart</button>
-        </div>
-      </div>
+    <div className="app">
+      <h1>YouTube Video Search</h1>
     </div>
   );
-}
+};
 
 export default App;
