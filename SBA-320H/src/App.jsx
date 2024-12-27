197afdd4e291b2a96e7325ad3ce8dcf44a1a10ce
Brian <briane0717@gmail.com>
2024-12-27 13:04:52 -0500

    correct sample products

--- SBA-320H/src/App.jsx
+++ SBA-320H/src/App.jsx
@@ -1,12 +1,34 @@
 import "./App.css";
 
+// Sample products
+
 function App() {
-  // Sample products
-  const products = [
-    { id: 1, name: "Mug", price: 30.0 },
-    { id: 2, name: "Hoodie", price: 50.0 },
-    { id: 3, name: "Cup", price: 20.0 },
-  ];
+  return (
+    <div className="App">
+      <h1>Express Yourself!</h1>
+
+      {/* Product List */}
+      <div className="product-list">
+        <div className="product-item">
+          <h3>Mug</h3>
+          <p>$$30.00</p>
+          <button>Add to Cart</button>
+        </div>
+
+        <div className="product-item">
+          <h3>Hoodie</h3>
+          <p>$50.00</p>
+          <button>Add to Cart</button>
+        </div>
+
+        <div className="product-item">
+          <h3>Cup</h3>
+          <p>$20.00</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
